ebbdf87296db341f7a8a9ba54245602f6cae2f1b
mpnsk <mpnsk@users.noreply.github.com>
2017-10-24 19:32:28 +0200

    Get url for fixed ean

--- src/main/java/com/paunoski/eantoasinserverjava/EanToAsinServerJavaApplication.java
+++ src/main/java/com/paunoski/eantoasinserverjava/EanToAsinServerJavaApplication.java
@@ -1,12 +1,21 @@
 package com.paunoski.eantoasinserverjava;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EanToAsinServerJavaApplication {
+public class EanToAsinServerJavaApplication implements CommandLineRunner {
+    @Autowired
+    JavaCodeSnippet javaCodeSnippet;
 
-	public static void main(String[] args) {
-		SpringApplication.run(EanToAsinServerJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EanToAsinServerJavaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(javaCodeSnippet.getUrl("9780465095001"));
+    }
 }
---
+++ src/main/java/com/paunoski/eantoasinserverjava/JavaCodeSnippet.java
@@ -0,0 +1,71 @@
+package com.paunoski.eantoasinserverjava;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * This class shows how to make a simple authenticated call to the
+ * Amazon Product Advertising API.
+ *
+ * See the README.html that came with this sample for instructions on
+ * configuring and running the sample.
+ */
+@Component
+public class JavaCodeSnippet {
+
+    /*
+     * Your Access Key ID, as taken from the Your Account page.
+     */
+    @Value("${amazon-product-advertising-api.access-key}")
+    private String ACCESS_KEY_ID;
+
+    /*
+     * Your Secret Key corresponding to the above ID, as taken from the
+     * Your Account page.
+     */
+    @Value("${amazon-product-advertising-api.secret-key}")
+    private String SECRET_KEY;
+
+    /*
+     * Use the end-point according to the region you are interested in.
+     */
+    @Value("${amazon-region-endpoint}")
+    private String ENDPOINT;
+
+
+    @Value("${amazon-associate-tag}")
+    private String ASSOCIATE_TAG;
+
+    String getUrl(String ean) {
+        /*
+         * Set up the signed requests helper.
+         */
+        SignedRequestsHelper helper;
+
+        try {
+            helper = SignedRequestsHelper.getInstance(ENDPOINT, ACCESS_KEY_ID, SECRET_KEY);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        String requestUrl = null;
+
+        Map<String, String> params = new HashMap<>();
+
+        params.put("Service", "AWSECommerceService");
+        params.put("Operation", "ItemLookup");
+        params.put("AWSAccessKeyId", ACCESS_KEY_ID);
+        params.put("AssociateTag", ASSOCIATE_TAG);
+        params.put("ItemId", ean);
+        params.put("IdType", "EAN");
+        params.put("ResponseGroup", "Images,ItemAttributes,Offers");
+        params.put("SearchIndex", "All");
+
+        requestUrl = helper.sign(params);
+        return requestUrl;
+    }
+}
